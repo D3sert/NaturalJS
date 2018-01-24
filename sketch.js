16081c2652277c46c7eb758d156072d22a278b85
D3sert <tschmid90@yahoo.de>
2018-01-24 15:44:12 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,15 @@
+function Data() {
+var url="https://docs.google.com/spreadsheets/d/e/2PACX-1vS5S7qoB493c9Av3MaCFqrVP7cvC5xaQsO7xVMnDkoH7vYcrI3LZnb_1qfHlB0wXucR1tnRcVqEpwzv/pub?gid=0&single=true&output=csv";
+
+xmlhttp=new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if(xmlhttp.readyState == 4 && xmlhttp.status==200){
+      document.Team.tName.value = xmlhttp.responseText;
+    }
+  };
+  xmlhttp.open("GET",url,true);
+  xmlhttp.send(null);
+  console.log(xmlhttp.responseText);
+}
+
 
